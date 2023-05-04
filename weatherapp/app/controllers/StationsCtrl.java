bfd566acc574b7ab44aeed101ef6764413c063ac
Robert Ferguson <robertferguson78@gmail.com>
2023-05-04 23:10:34 +0100

    Add delete Station Reading functionality
    Delete station reading functionality for logged in user and redirect back to reading list

--- weatherapp/app/controllers/StationsCtrl.java
+++ weatherapp/app/controllers/StationsCtrl.java
@@ -23,4 +23,14 @@ public class StationsCtrl extends Controller {
     station.save();
     redirect("/stations/" + id);
   }
+
+  public static void deleteReading(Long id, Long readingid) {
+    Station station = Station.findById(id);
+    Reading reading = Reading.findById(readingid);
+    Logger.info("Removing " + reading.id);
+    station.readings.remove(reading);
+    station.save();
+    reading.delete();
+    redirect("/stations/" + id);
+  }
 }
