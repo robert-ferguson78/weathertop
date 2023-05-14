169b7adccdb4e2e4e80084024ddd7c71fb1b5221
Robert Ferguson <robertferguson78@gmail.com>
2023-05-14 13:39:13 +0100

    Added mobile menu to logged out user and applied code style to files
    Added menu bar for logged out users menu and also applied automatic code indentation style on files

--- app/models/Reading.java
+++ app/models/Reading.java
@@ -6,8 +6,7 @@ import javax.persistence.Entity;
 import java.util.Date;
 
 @Entity
-public class Reading extends Model
-{
+public class Reading extends Model {
   public int code;
   public double temperature;
   public double windSpeed;
@@ -15,8 +14,7 @@ public class Reading extends Model
   public float windDirection;
   public Date date;
 
-  public Reading(int code, double temperature, double windSpeed, int pressure, float windDirection, Date date)
-  {
+  public Reading(int code, double temperature, double windSpeed, int pressure, float windDirection, Date date) {
     this.code = code;
     this.temperature = temperature;
     this.windSpeed = windSpeed;
@@ -30,34 +28,48 @@ public class Reading extends Model
   public int getCode() {
     return code;
   }
+
   public double getTemperature() {
     return temperature;
   }
+
   public double getWindSpeed() {
     return windSpeed;
   }
+
   public int getPressure() {
     return pressure;
   }
-  public float getWindDirection() { return windDirection;}
-  public Date getDate() { return date;}
+
+  public float getWindDirection() {
+    return windDirection;
+  }
+
+  public Date getDate() {
+    return date;
+  }
 
   //setters
   public void setCode(int code) {
     this.code = code;
   }
+
   public void setTemperature(double temperature) {
     this.temperature = temperature;
   }
+
   public void setWindSpeed(float windSpeed) {
     this.windSpeed = windSpeed;
   }
+
   public void setPressure(int pressure) {
     this.pressure = pressure;
   }
+
   public void setWindDirection(float windDirection) {
     this.windDirection = windDirection;
   }
+
   public void setDate(Date date) {
     this.date = date;
   }
