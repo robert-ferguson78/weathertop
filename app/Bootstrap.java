7c516a8403a70eebe527101cd9cf365b366ef47e
Robert Ferguson <robertferguson78@gmail.com>
2023-05-18 21:37:21 +0100

    Update Bootstrap for loading YAML data
    Added IF statement to check is there are no users so YAML data can be imported, this was to stop the Homer user getting imported multiple times when InteliJ was stopped and started

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -7,10 +7,11 @@ import play.test.*;
 import models.*;
 
 @OnApplicationStart
-public class Bootstrap extends Job
-{
-  public void doJob()
-  {
-    Fixtures.loadModels("data.yml");
+public class Bootstrap extends Job {
+
+  public void doJob() {
+    if (Member.count() == 0) {
+      Fixtures.loadModels("data.yml");
+    }
   }
 }
